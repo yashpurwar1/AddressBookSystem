d6d1cd1f21674967e1121b388a81c75aad3f465c
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-11 00:29:47 +0530

    Added: Variables and genrated getters and setters

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,9 +1,76 @@
 package addressBookSystem;
 
 public class AddressBookSystem {
+	private String firstName, lastName, address, city, state, email;
+	private long zip, phoneNo;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getZip() {
+		return zip;
+	}
+
+	public void setZip(long zip) {
+		this.zip = zip;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
 	}
+
 
 }
