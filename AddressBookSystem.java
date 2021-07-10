3ea51a98e5aa6627543acce16a8be1c271f3aa9a
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-11 02:27:15 +0530

    Added: Functionality to edit a record

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -82,8 +82,7 @@ public class AddressBookSystem {
 		this.phoneNo = phoneNo;
 	}
 
-	public void addDetail() {
-		Scanner scanner = new Scanner(System.in);
+	public void addDetail(Scanner scanner) {
 		AddressBookSystem detail = new AddressBookSystem();
 		System.out.println("Enter first name");
 		detail.setFirstName(scanner.next());
@@ -102,14 +101,83 @@ public class AddressBookSystem {
 		System.out.println("Enter phone number");
 		detail.setPhoneNo(scanner.nextLong());
 		detailArray.add(detail);
-		scanner.close();
+	}
+	
+	public void edit(Scanner scanner) {
+		
+		System.out.println("Confirm your first name to edit details: ");
+		String name = scanner.next();
+
+		for (int i = 0; i < detailArray.size(); i++) {
+			if (detailArray.get(i).getFirstName().equals(name)) {
+				System.out.println("Select form below to change: ");
+				System.out.println("\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Email\n7.Zip\n8.Phone number");
+				int edit = scanner.nextInt();
+
+				switch (edit) {
+				case 1:
+					System.out.println("Enter first name");
+					detailArray.get(i).setFirstName(scanner.next());
+					break;
+				case 2:
+					System.out.println("Enter Last name");
+					detailArray.get(i).setLastName(scanner.next());
+					break;
+				case 3:
+					System.out.println("Enter address");
+					detailArray.get(i).setAddress(scanner.next());
+					break;
+				case 4:
+					System.out.println("Enter city");
+					detailArray.get(i).setCity(scanner.next());
+					break;
+				case 5:
+					System.out.println("Enter state");
+					detailArray.get(i).setState(scanner.next());
+					break;
+				case 6:
+					System.out.println("Enter email");
+					detailArray.get(i).setEmail(scanner.next());
+					break;
+				case 7:
+					System.out.println("Enter Zip");
+					detailArray.get(i).setZip(scanner.nextLong());
+					break;
+				case 8:
+					System.out.println("Enter phone number");
+					detailArray.get(i).setPhoneNo(scanner.nextLong());
+					break;
+				}
+
+			} else
+				System.out.println("Enter valid First name");
+		}
+
 	}
 
 	public static void main(String[] args) {
+		int i=0;
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to Address Book Program");
 		AddressBookSystem detail = new AddressBookSystem();
-		detail.addDetail();
-		detail.output();
+		while (i==0) {
+			System.out.println("What you want to do: ");
+			System.out.println("1.Add details.\n2.Edit details.");
+			int choose = scanner.nextInt();
+			switch (choose) {
+			case 1:
+				detail.addDetail(scanner);
+				break;
+			case 2:
+				detail.edit(scanner);
+				break;
+			default:
+				i=1;
+				System.out.println("Wrong option");
+				detail.output();
+				break;
+			}
+		}
 	}
 
 }
